6750f955984b554af01dbfaf4138dceb5445918e
Ovidiu Nitan <ovidiu@appscend.com>
2017-10-18 11:52:42 +0300

    Just some logs

--- tv-maxe/settingsmanager.py
+++ tv-maxe/settingsmanager.py
@@ -1,3 +1,4 @@
+import logging
 from PyQt5.QtCore import QSettings
 
 DEFAULT_SUBSCRIPTIONS = [
@@ -5,9 +6,12 @@ DEFAULT_SUBSCRIPTIONS = [
     [True, "http://tv-maxe.org/subscriptions/v2/International.db"]
 ]
 
+log = logging.getLogger(__name__)
+
 class SettingsManager(QSettings):
     def __init__(self):
         super().__init__()
+        log.debug("Loaded configuration from {0}".format(self.fileName()))
 
     def get_subscriptions(self):
         return self.value("subscriptions", DEFAULT_SUBSCRIPTIONS)
